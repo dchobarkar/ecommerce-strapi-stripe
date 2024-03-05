531c92c0a53ec2498cb68dae8296d07f4b6c321c
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-05 17:48:11 +0530

    add hero banner section

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Banner from "./Banner/Banner";
 import "./Home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;

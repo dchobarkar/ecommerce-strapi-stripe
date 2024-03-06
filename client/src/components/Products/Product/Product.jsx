78e620d9b04084967b392bc64469afb675551ee6
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-06 16:29:02 +0530

    integrate homepage products api

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-import prod from "../../../assets/products/earbuds-prod-1.webp";
 import "./Product.scss";
 
 const Product = ({ data, id }) => {
   return (
     <div className="product-card">
       <div className="thumbnail">
-        <img src={prod} alt="" />
+        <img
+          src={
+            process.env.REACT_APP_STRIPE_APP_DEV_URL +
+            data.image.data[0].attributes.url
+          }
+          alt=""
+        />
       </div>
       <div className="prod-details">
-        <span className="name">Product Name</span>
-        <span className="price">&#8377; 499</span>
+        <span className="name">{data.title}</span>
+        <span className="price">&#8377;{data.price}</span>
       </div>
     </div>
   );
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,20 +1,14 @@
 import Product from "./Product/Product";
 import "./Products.scss";
 
-const Products = ({ innerPage, headingText }) => {
+const Products = ({ products, innerPage, headingText }) => {
   return (
     <div className="products-container">
-      <div className="sec-heading">{headingText}</div>
-
-      <div className="products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+      {!innerPage && <div className="sec-heading">{headingText}</div>}
+      <div className={`products ${innerPage ? "innerPage" : ""}`}>
+        {products?.data?.map((item) => (
+          <Product key={item.id} id={item.id} data={item.attributes} />
+        ))}
       </div>
     </div>
   );

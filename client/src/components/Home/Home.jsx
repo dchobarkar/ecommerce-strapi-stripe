12469fdf27ef96636d6a02e0714db218cd81991d
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-06 16:23:58 +0530

    setup api for homepage

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useContext } from "react";
+
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
+import { Context } from "../../utils/context";
 import "./Home.scss";
 
 const Home = () => {
+  const { categories, setCategories, products, setProducts } =
+    useContext(Context);
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
+  }, []);
+
+  const getProducts = () => {
+    fetchDataFromApi("/api/products?populate=*").then((res) =>
+      setProducts(res)
+    );
+  };
+
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories?populate=*").then((res) =>
+      setCategories(res)
+    );
+  };
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category />
-          <Products headingText="Popular Products" />
+          <Category categories={categories} />
+          <Products products={products} headingText="Popular Products" />
         </div>
       </div>
     </div>
